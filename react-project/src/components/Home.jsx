51c3aba78c9143fb0a09c823088247f740a74057
Shivani-Banduni <shivanibanduni009@gmail.com>
2023-09-18 13:20:36 +0530

    fetch data

--- react-project/src/components/Home.jsx
+++ react-project/src/components/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import axios from 'axios'
 const Home = () => {
+    const [data, setdata]=useState([])
+    axios.get('https://jsonplaceholder.typicode.com/users').then((r)=>console.log(r.data))
     console.log("home")
+
     return (
         <div>
             <h1>Home</h1>
